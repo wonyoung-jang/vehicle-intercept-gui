e4d8084b096bd4675439eee2c986b7cd3f395bad
Wonyoung Jang <114602935+wonyoung-jang@users.noreply.github.com>
2024-07-19 17:52:48 -0400

    increase: chart display size

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from PySide6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout, 
                                QLabel, QDoubleSpinBox, QPushButton, QTabWidget,
                                QGroupBox, QFormLayout)
-from PySide6.QtCharts import QChart, QChartView, QLineSeries, QValueAxis
+from PySide6.QtCharts import QChart, QChartView, QLineSeries, QValueAxis, QScatterSeries
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QColor, QPen
 
@@ -125,9 +125,9 @@ class DroneInterceptWindow(QWidget):
             
             self.result_label.setText(self.result_label.text() + '\n' + '\n'.join(suggestions))        
             
-        self.update_chart(miles_delay_distance, intercept_possible)
+        self.update_chart(mins_drone_speed, miles_delay_distance, intercept_possible)
                 
-    def update_chart(self, miles_delay_distance, intercept_possible):
+    def update_chart(self, mins_drone_speed, miles_delay_distance, intercept_possible):
         chart = QChart()
         chart.setTitle("Drone Intercept Visualization")
 
@@ -135,13 +135,21 @@ class DroneInterceptWindow(QWidget):
         radar_series = QLineSeries()
         radar_series.setName("Radar Range")
         radar_series.append(0, self.radar_range.value())
-        radar_series.append(self.reaction_time.value(), self.radar_range.value())
+        radar_series.append(self.reaction_time.value() * 2, self.radar_range.value())
 
         # Series for drone position
         drone_series = QLineSeries()
         drone_series.setName("Drone Position")
         drone_series.append(0, 0)
-        drone_series.append(self.reaction_time.value(), miles_delay_distance)
+        max_time = max(self.reaction_time.value() * 2, self.radar_range.value() / mins_drone_speed)
+        drone_series.append(max_time, max_time * mins_drone_speed)
+                
+        # Intercept point
+        intercept_series = QScatterSeries()
+        intercept_series.setName("Intercept Point")
+        if intercept_possible:
+            intercept_time = (self.radar_range.value() - miles_delay_distance) / mins_drone_speed
+            intercept_series.append(intercept_time, self.radar_range.value())
 
         # Set colors based on intercept possibility
         color = QColor(Qt.green) if intercept_possible else QColor(Qt.red)
@@ -149,21 +157,29 @@ class DroneInterceptWindow(QWidget):
         pen.setWidth(3)
         radar_series.setPen(pen)
         drone_series.setPen(pen)
+        intercept_series.setColor(QColor(Qt.blue))
+        intercept_series.setMarkerSize(15)
 
         chart.addSeries(radar_series)
         chart.addSeries(drone_series)
+        chart.addSeries(intercept_series)
 
         axis_x = QValueAxis()
         axis_x.setTitleText("Time (minutes)")
+        axis_x.setRange(0, max_time * 1.1)
         chart.addAxis(axis_x, Qt.AlignBottom)
         radar_series.attachAxis(axis_x)
         drone_series.attachAxis(axis_x)
+        intercept_series.attachAxis(axis_x)
 
         axis_y = QValueAxis()
         axis_y.setTitleText("Distance (miles)")
+        max_distance = max(self.radar_range.value(), max_time * mins_drone_speed)
+        axis_y.setRange(0, max_distance * 1.1)
         chart.addAxis(axis_y, Qt.AlignLeft)
         radar_series.attachAxis(axis_y)
         drone_series.attachAxis(axis_y)
+        intercept_series.attachAxis(axis_y)
 
         self.chart_view.setChart(chart)
     
@@ -260,34 +276,54 @@ class CarCollisionWindow(QWidget):
     def update_chart(self, time_to_collision):
         chart = QChart()
         chart.setTitle("Car Collision Visualization")
-
+        max_time = time_to_collision * 1.5 if time_to_collision > 0 else 1
+        
         # Series for Car A
         series_a = QLineSeries()
         series_a.setName("Car A")
         series_a.append(0, 0)
-        series_a.append(time_to_collision, self.speed_car_a.value() * time_to_collision)
+        series_a.append(max_time, self.speed_car_a.value() * max_time)
 
         # Series for Car B
         series_b = QLineSeries()
         series_b.setName("Car B")
         initial_b_position = UnitConverter.feet_to_miles(self.initial_distance.value())
         series_b.append(0, initial_b_position)
-        series_b.append(time_to_collision, initial_b_position + self.speed_car_b.value() * time_to_collision)
-
+        series_b.append(max_time, initial_b_position + self.speed_car_b.value() * max_time)
+        
+        # Collision point
+        collision_series = QScatterSeries()
+        collision_series.setName("Collision Point")
+        if time_to_collision > 0:
+            collision_series.append(time_to_collision, self.speed_car_a.value() * time_to_collision)
+            
         chart.addSeries(series_a)
         chart.addSeries(series_b)
+        chart.addSeries(collision_series)
 
         axis_x = QValueAxis()
         axis_x.setTitleText("Time (hours)")
+        axis_x.setRange(0, max_time)
         chart.addAxis(axis_x, Qt.AlignBottom)
         series_a.attachAxis(axis_x)
         series_b.attachAxis(axis_x)
+        collision_series.attachAxis(axis_x)
 
         axis_y = QValueAxis()
         axis_y.setTitleText("Distance (miles)")
+        max_distance = max(self.speed_car_a.value() * max_time, 
+                    initial_b_position + self.speed_car_b.value() * max_time)
+        axis_y.setRange(0, max_distance * 1.1)
         chart.addAxis(axis_y, Qt.AlignLeft)
         series_a.attachAxis(axis_y)
         series_b.attachAxis(axis_y)
+        collision_series.attachAxis(axis_y)
+        
+        # Set colors
+        series_a.setPen(QPen(QColor(Qt.blue), 2))
+        series_b.setPen(QPen(QColor(Qt.red), 2))
+        collision_series.setColor(QColor(Qt.green))
+        collision_series.setMarkerSize(15)
 
         self.chart_view.setChart(chart)
 

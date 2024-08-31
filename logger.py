f70de79e667fccc182f76c73f75460532a3d1f14
Wonyoung Jang <114602935+wonyoung-jang@users.noreply.github.com>
2024-08-31 10:35:11 -0400

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,10 @@
+import logging
+
+def create_logger(name, level=logging.INFO):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
